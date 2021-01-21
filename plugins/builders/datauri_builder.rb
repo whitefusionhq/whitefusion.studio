70624855999ce7e8b72a8d62411bc4ee7cb974eb
Jared White <jared@jaredwhite.com>
2021-01-21 13:25:57 -0800

    Use data URI for thumbnail logo in navbar

---
+++ plugins/builders/datauri_builder.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class DatauriBuilder < SiteBuilder
+  def build
+    liquid_tag "datauri", :datauri
+    helper "datauri", :datauri
+  end
+
+  def datauri(image_path, tag=nil)
+    full_path = site.in_source_dir("images", image_path.strip)
+    data = File.read(full_path)
+    encoded_data = Base64.strict_encode64(data)
+    "data:image/png;base64,#{encoded_data}"
+  end
+end
