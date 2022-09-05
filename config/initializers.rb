af6e60d77b27806d59a53dfcfb83022a164007fa
Jared White <jared@jaredwhite.com>
2022-09-05 15:47:56 -0700

    Strip out Sidekiq testing and streamline everything

--- config/initializers.rb
+++ config/initializers.rb
@@ -1,20 +1,13 @@
 Bridgetown.configure do |config|
-  init :dotenv
-
-  config.autoload_paths << "jobs"
-
-  init :ssr do
-    setup -> site do
-      puts "This is my #{site}"
-    end
-  end
-  init :"bridgetown-routes"
-  init :"roda-turbo"
-
   permalink "pretty"
   timezone "America/Los_Angeles"
 
-  # reflect :stripe
+  init :dotenv
+
+  init :ssr
+  init :"bridgetown-routes"
+  init :"roda-turbo"
+
   init :stripe, api_key: ENV["STRIPE_API_KEY"]
 
   only :server do
@@ -24,24 +17,6 @@ Bridgetown.configure do |config|
       password ENV["SENDGRID_API_KEY"]
     end
   end
-
-  init :sidekiq, dev_cli_options: "-v"
-
-  only :static, :console do
-    puts "static and console!"
-  end
-
-  only :rake do
-    puts "I'm in a Rake context!"
-  end
-
-  except :static, :console do
-    hook :site, :after_init do |site|
-      puts "Hello, I'm a #{site}! Thread #{Thread.current.native_thread_id}"
-    end
-  end
-
-  # init :"bridgetown-activerecord"
 end
 
 Bridgetown.initializer :stripe do |api_key:|
--- jobs/tryout_job.rb
+++
@@ -1,8 +0,0 @@
-class TryoutJob
-  include Sidekiq::Job
-  sidekiq_options queue: "wowee"
-
-  def perform(*args)
-    puts "Wow man! #{bridgetown_site.collections.posts} #{args}"
-  end
-end
